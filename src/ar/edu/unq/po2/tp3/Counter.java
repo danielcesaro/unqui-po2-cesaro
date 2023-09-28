b987890f73e910b713d53f318f1120be74a0da01
danie <danie@DESKTOP-AQ1JJKJ>
2023-09-28 16:53:36 -0300

    Pruebas

--- src/ar/edu/unq/po2/tp3/Counter.java
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -1,5 +1,19 @@
 package ar.edu.unq.po2.tp3;
 
-public class Counter {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Counter {
+	List<Integer> counter = new ArrayList<Integer>();
+	
+	int getEvenOcurrences() {
+		int n = 0;
+		
+		counter.forEach(e -> {
+			if ((e % 2) == 0) {
+				n++;
+			});
+		
+		return n;
+	}
 }
