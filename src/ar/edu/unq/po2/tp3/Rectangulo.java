bde45234be6098c739eaa02423d0125fe800331d
danie <danie@192.168.0.168>
2023-10-06 07:52:14 -0300

    Agregado Rectangulo

--- src/ar/edu/unq/po2/tp3/Rectangulo.java
+++ src/ar/edu/unq/po2/tp3/Rectangulo.java
@@ -1,5 +1,15 @@
 package ar.edu.unq.po2.tp3;
 
 public class Rectangulo {
-
+	private Point esqSupIzq;
+	private Point esqInfDer;
+	
+	public Rectangulo (Point a, Point b) {
+		esqSupIzq = a;
+		esqInfDer = b;
+	}
+	
+	public int area() {
+		return 0;
+	}
 }
